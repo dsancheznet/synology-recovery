2a4e537f55059c0d3978d59230baa5f56650adfd
Dominik Sánchez <4673828+dsancheznet@users.noreply.github.com>
2021-02-25 09:03:17 +0000

    Update synology_shr_recovery.sh
    Added auto configuration of samba server

--- synology_shr_recovery.sh
+++ synology_shr_recovery.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
 
-echo "Starting recovery of synology SHR disks"
-
-echo "Installing neccessary software"
-apt install mdadm lvm2 samba
-echo "Loading Software Raid"
+# Install neccessary packets and mount disks
+echo "*****Starting recovery of synology SHR disks"
+echo "*****Installing neccessary software"
+apt install -y mdadm lvm2 samba mc
+echo "*****Loading Software Raid"
 mdadm -Asf
-echo "Activate volume group"
+echo "*****Activate volume group"
 vgchange -ay
+
+# Deternmine where the hds have been mounted 
+echo "*****Determine mount point"
+MP=`mount | grep media | grep -o "/media/[a-zA-Z0-9./]*"`
+
+# Configure SAMBA
+echo "*****Configure SAMBA"
+cat << EOF >> /etc/samba/smb.conf
+
+[SYNOLOGY]
+    comment = Synology HD Volume Group
+    path = $MP
+    browseable = yes
+    force user = root
+    force group = root
+    read only = yes
+    guest ok = yes
+EOF
+
+#Restart SAMBA
+echo "*****Restart SAMBA"
+service smbd restart
+
 echo "Done."
